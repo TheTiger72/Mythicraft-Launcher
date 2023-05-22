dc2ac9fbf07a725f64b488cb4e903c0eefabfb4d
Zaworski Luigi <luigi.zaworski@icloud.com>
2023-05-22 17:50:44 +0200

    t

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/TheTiger72/Nebula/1516d95532459a985f979264ab109e158106125c/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/TheTiger72/Nebula/master/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
